33f3aede2bdb263463f49f66ee397078573403a5
chewchew <johan@server.com>
2017-05-16 11:19:53 +0200

    fixed node crash

--- lab-3/workerpool.erl
+++ lab-3/workerpool.erl
@@ -40,10 +40,12 @@ worker(Master,Pool) ->
 	end.
 
 init_workers(Master,Pool,Nodes,NWorkers) ->
+	process_flag(trap_exit,true),
 	lists:flatten(lists:map(
 			fun(Node) -> 
-				[spawn(Node,
-					fun() -> 
+				[spawn_link(Node,
+					fun() ->
+						dets:open_file(web,[{file,"web.dat"}]),
 						worker(Master,Pool) 
 					end) || _ <- lists:seq(1,NWorkers)] 
 			end, Nodes)).
@@ -53,4 +55,13 @@ start(Jobs) ->
 	Nodes  = nodes(),
 	Pool = start_job_pool(Jobs),
 	Workers = init_workers(Master,Pool,Nodes,?N_WORKERS),
-	[receive {JobId,R} -> R end || {JobId,_} <- Jobs].
+	lists:filter(fun(X) -> case X of sig -> false; _ -> true end end,[receive 
+			{JobId,R} -> R;
+			{'EXIT',From,normal} ->
+				io:format("Node ~w finished with no problem.~n",[From]),
+				sig;
+			{'EXIT',From,Reason} -> 
+				io:format("Node ~w ~w, running job on main thread.~n",[From,Reason]),
+				spawn(backup,worker,[Master,Pool]),
+				sig
+		end || {JobId,_} <- Jobs]).
